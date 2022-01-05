945b5ffb5a3291b1f297c837ab2a748eb0ec1e60
Johan Vlassak <johan.vlassak@gmail.com>
2022-01-05 18:09:03 -0500

    details for yt video

---
+++ main_code.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+
+
