65fe995a9c9b4879969becd88e307ea5162a4e29
Johan Vlassak <johan.vlassak@gmail.com>
2022-01-05 19:58:55 -0500

    finished for now, saves mp3 to main directoyry

--- main_code.py
+++ main_code.py
@@ -3,6 +3,7 @@ import sys
 import os
 from moviepy.editor import *
 
+
 #ask for the link from user
 link = input("Enter the link of YouTube video you want to download:  ")
 
@@ -13,10 +14,26 @@ except:
 
 #Getting the highest resolution possible
 ys = yt.streams.get_highest_resolution()
+#Starting download, as a mp4
 
-#Starting download
 print("Downloading...")
 ys.download()
-video = VideoFileClip(os.path.join("path","to","movie.mp4"))
-video.audio.write_audiofile(os.path.join("path","to","movie_sound.mp3"))
+
+filename = ys.title
+slash = '\\'
+mp4_name = os.getcwd() + slash + filename + '.mp4'
+mp3_name = os.getcwd() + slash + filename + '.mp3'
+#converst the mp4 to an mp3 and downloads that.
+videoclip = VideoFileClip(mp4_name)
+
+audioclip = videoclip.audio
+audioclip.write_audiofile(mp3_name)
+
+audioclip.close()
+videoclip.close()
+
 print("Download completed")
+
+
+
+
